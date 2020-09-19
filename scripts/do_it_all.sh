47352e759e2d9e5e9f786dfe4f239b635958642e
Ian Page Hands <iphands@gmail.com>
2020-09-19 16:57:06 -0400

    debug

--- scripts/do_it_all.sh
+++ scripts/do_it_all.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
+
+echo "#### Downloading csv data:"
 bash scripts/get_exported.sh
+ls -lh exported/
+
+echo "#### Running build:"
 bash scripts/build.sh
+ls -lh build/
+
+echo "#### Deploying to ahands.org:"
 if [ -z "$TRAVIS" ] ; then bash scripts/deploy.sh ; fi
+
+echo "#### Deploying www.randonneur.me:"
 bash scripts/ftp_deploy.sh
