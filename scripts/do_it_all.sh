b0ab0c0cd8bfda6fc5e318daa015c21c17ad6c63
Ian Page Hands <iphands@gmail.com>
2021-09-15 12:42:08 -0400

    undo travis -> ahands

--- scripts/do_it_all.sh
+++ scripts/do_it_all.sh
@@ -7,14 +7,14 @@ time bash scripts/get_exported.sh
 echo "#### Running build:"
 time bash scripts/build.sh
 
-if [ -z "$TRAVIS" ]
-then
-    echo "#### Deploying to ahands.org:"
-    time bash scripts/deploy.sh
-fi
+# if [ -z "$TRAVIS" ]
+# then
+#    echo "#### Deploying to ahands.org:"
+#    time bash scripts/deploy.sh
+# fi
 
-if [ -z "$TRAVIS" ] || [ "$TRAVIS_BRANCH" == "main" ]
-then
-    echo "#### Deploying to www.randonneur.me:"
-    time bash scripts/ftp_deploy.sh
-fi
+# if [ -z "$TRAVIS" ] || [ "$TRAVIS_BRANCH" == "main" ]
+# then
+echo "#### Deploying to www.randonneur.me:"
+time bash scripts/ftp_deploy.sh
+# fi
